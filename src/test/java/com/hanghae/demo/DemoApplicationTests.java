b00d58045f3068beef782ffe7012abe1b7d7538e
JeongA Lee <201424077b@gmail.com>
2024-11-14 16:59:39 +0900

    [feat] init project

---
+++ src/test/java/com/hanghae/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
